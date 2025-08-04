578a460cc2a628983aad3b63b1c1fa9052d8c556
xonier-mridul <mridulsaklanixonier@gmail.com>
2025-08-04 22:08:48 +0530

    fix: token

--- Frontend/src/app/(admin)/verify-otp/page.jsx
+++ Frontend/src/app/(admin)/verify-otp/page.jsx
@@ -91,13 +91,7 @@ const Page = () => {
         });
        
         console.log("userData: ", response.data)
-        Cookies.set("accessToken", response.data.accessToken, {
-          httpOnly: true,
-  secure: true,               // only over HTTPS
-  sameSite: 'None',         // or 'None' if cross-domain
-  expires: 2,                 // days, or use a Date object
-  path: '/',                  // root path
-})
+        
 
         window.localStorage.removeItem("verify-login-email");
         window.localStorage.removeItem("verify-login-password");
