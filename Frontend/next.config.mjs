79c6dd0384c88d172a0719e61f7cdf8aff14a300
xonier-mridul <mridulsaklanixonier@gmail.com>
2025-08-28 16:30:06 +0530

    fix: some issue

--- Frontend/next.config.mjs
+++ Frontend/next.config.mjs
@@ -9,14 +9,14 @@ const nextConfig = {
         unoptimized: true,
 
     },
-    async rewrites() {
-        return [
-            {
-                source: "/api/:path*",
-                destination: "http://localhost:3000/api/:path*",
-            },
-        ];
-    },
+    // async rewrites() {
+    //     return [
+    //         {
+    //             source: "/api/:path*",
+    //             destination: "http://localhost:3000/api/:path*",
+    //         },
+    //     ];
+    // },
     
 };
 
