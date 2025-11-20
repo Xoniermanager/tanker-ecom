59562f1a69a4558f930d3c492e641ba905969e82
xonier-mridul <mridulsaklanixonier@gmail.com>
2025-11-20 21:30:14 +0530

    fix: bugs

--- Frontend/components/admin/quote/QuoteTable.jsx
+++ Frontend/components/admin/quote/QuoteTable.jsx
@@ -7,6 +7,7 @@ import { MdDeleteOutline } from "react-icons/md";
 import { BsCartX } from "react-icons/bs";
 import Link from "next/link";
 import DataNotFount from "../../common/DataNotFount";
+
 const QuoteTable = ({
   quoteData,
   setTotalPages,
@@ -14,14 +15,15 @@ const QuoteTable = ({
   currentPage,
   setCurrentPage,
   setPageLimit,
-  handleDeleteQuote
+  handleDeleteQuote,
 }) => {
   if (quoteData?.length <= 0) {
     return <DataNotFount />;
   }
+
   return (
     <>
-      <div className="min-h-screen bg-[#f4f2ff] ">
+      <div className="min-h-screen bg-[#f4f2ff]">
         <div className="mb-4 bg-white p-4 px-6 rounded-lg">
           <h2 className="text-2xl font-semibold text-black">Queries List</h2>
           <div className="text-sm text-gray-500 mt-1">
@@ -31,7 +33,7 @@ const QuoteTable = ({
 
         <div className="rounded-xl overflow-x-auto">
           <table className="min-w-full table-auto text-sm text-left border-separate border-spacing-y-4">
-            <thead className="bg-[#f4f2ff]  uppercase text-xs">
+            <thead className="bg-[#f4f2ff] uppercase text-xs">
               <tr>
                 <th className="px-6 py-4 font-medium">Name</th>
                 <th className="px-6 py-4 font-medium">Email</th>
@@ -50,21 +52,19 @@ const QuoteTable = ({
                     <td className="px-6 py-6 capitalize">
                       {order.firstName} {order.lastName}
                     </td>
-                    <td className="px-6 py-6 ">
+                    <td className="px-6 py-6">
                       <span className="bg-orange-500 px-2 py-1 text-white rounded-lg text-[12px] tracking-wide">
                         {order.email}
                       </span>
                     </td>
                     <td className="px-6 py-6">
-                      {" "}
                       <span className="bg-green-50 text-green-500 px-3 py-1 text-sm rounded-lg">
-                        {" "}
                         {order.phone}
                       </span>
                     </td>
                     <td className="px-6 py-6">
                       <span className="text-white bg-purple-900 px-3 py-1 text-[12px] rounded-lg">
-                        {new Date(order.createdAt).toLocaleDateString("en-NZ", {
+                        {new Date(order?.createdAt).toLocaleDateString("en-NZ", {
                           weekday: "short",
                           month: "short",
                           day: "numeric",
@@ -75,59 +75,93 @@ const QuoteTable = ({
 
                     <td className="px-6 py-6 text-xl text-gray-500 flex items-center justify-center gap-2">
                       <Link
-                        href={`quote/detail/${order._id}`}
-                        className="flex items-center justify-center h-8 w-8 rounded-lg text-white bg-orange-500"
+                        href={`quote/detail/${order?._id}`}
+                        className="flex items-center justify-center h-8 w-8 rounded-lg text-white bg-orange-500 hover:bg-orange-600"
                       >
                         <FaEye />
                       </Link>
-                      <button onClick={()=>handleDeleteQuote(order._id, `${order.firstName} ${order.lastName}`)} className="bg-red-500 h-8 w-8 rounded-lg text-white flex items-center justify-center">
-                          <MdDeleteOutline className=""/>
+                      <button
+                        onClick={() =>
+                          handleDeleteQuote(
+                            order._id,
+                            `${order.firstName} ${order.lastName}`
+                          )
+                        }
+                        className="bg-red-500 hover:bg-red-600 h-8 w-8 rounded-lg text-white flex items-center justify-center"
+                      >
+                        <MdDeleteOutline />
                       </button>
                     </td>
                   </tr>
                 ))
               ) : (
                 <tr className="bg-white rounded-xl overflow-hidden shadow">
-                  <td colSpan={6} className="p-4 text-center">
-                    {" "}
+                  <td colSpan={5} className="p-4 text-center">
                     <p className="flex items-center gap-2 justify-center">
-                      {" "}
-                      <BsCartX className="text-lg text-orange-400" /> Queries Data
-                      not found{" "}
+                      <BsCartX className="text-lg text-orange-400" /> Queries
+                      Data not found
                     </p>
                   </td>
                 </tr>
               )}
             </tbody>
           </table>
-          <div className="flex items-center gap-4 justify-center mt-6">
-            {[...Array(totalPages)].map((item, index) => (
+
+         
+          {totalPages > 1 && (
+            <div className="flex items-center gap-4 justify-center mt-10">
               <button
-                className={` ${
-                  currentPage === index + 1
-                    ? "bg-orange-400 text-white"
-                    : "bg-[#f6e7d3]"
-                } hover:bg-orange-400 hover:text-white  h-12 w-12 rounded-full border-white text-purple-950  font-bold border-1 border-dashed text-lg`}
-                key={index}
-                onClick={() => setCurrentPage(index + 1)}
+                className="h-12 w-12 rounded-full border-white bg-[#42666f] hover:bg-[#334f56] disabled:bg-[#588c99] font-bold border-1 border-dashed text-white flex items-center justify-center text-2xl rotate-180"
+                onClick={() => setCurrentPage(Number(currentPage) - 1)}
+                disabled={currentPage === 1}
               >
-                {index + 1}
+                <IoArrowForward />
               </button>
-            ))}
-            <button
-              disabled={
-                quoteData?.length <= 0 ||
-                Number(totalPages) === Number(currentPage)
-              }
-              className={`h-12 w-12 rounded-full border-white bg-[#42666f] hover:bg-[#334f56] disabled:bg-[#507b86c5] font-bold border-1 border-dashed text-white flex items-center justify-center text-2xl ${
-                quoteData?.length <= 0 && "hidden"
-              }`}
-              onClick={() => setCurrentPage(Number(currentPage) + 1)}
-            >
-              {" "}
-              <IoArrowForward />{" "}
-            </button>
-          </div>
+
+              {(() => {
+                let startPage, endPage;
+
+                if (totalPages <= 3) {
+                  startPage = 1;
+                  endPage = totalPages;
+                } else if (currentPage === 1) {
+                  startPage = 1;
+                  endPage = 3;
+                } else if (currentPage === totalPages) {
+                  startPage = totalPages - 2;
+                  endPage = totalPages;
+                } else {
+                  startPage = currentPage - 1;
+                  endPage = currentPage + 1;
+                }
+
+                return [...Array(endPage - startPage + 1)].map((_, index) => {
+                  const pageNumber = startPage + index;
+                  return (
+                    <button
+                      className={`${
+                        currentPage === pageNumber
+                          ? "bg-orange-400 text-white"
+                          : "bg-[#f6e7d3]"
+                      } hover:bg-orange-400 hover:text-white h-12 w-12 rounded-full border-white text-purple-950 font-bold border-1 border-dashed text-lg`}
+                      key={pageNumber}
+                      onClick={() => setCurrentPage(pageNumber)}
+                    >
+                      {pageNumber}
+                    </button>
+                  );
+                });
+              })()}
+
+              <button
+                className="h-12 w-12 rounded-full border-white bg-[#42666f] hover:bg-[#334f56] disabled:bg-[#588c99] font-bold border-1 border-dashed text-white flex items-center justify-center text-2xl"
+                onClick={() => setCurrentPage(Number(currentPage) + 1)}
+                disabled={totalPages === currentPage}
+              >
+                <IoArrowForward />
+              </button>
+            </div>
+          )}
         </div>
       </div>
     </>
