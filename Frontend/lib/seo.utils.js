e740f9f7686d618114f1df2677426321761c2f65
xonier-mridul <mridulsaklanixonier@gmail.com>
2026-02-04 18:17:49 +0530

    fix: catching in next

--- Frontend/lib/seo.utils.js
+++ Frontend/lib/seo.utils.js
@@ -2,9 +2,7 @@
 
 export async function getSiteSettings() {
   try {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/site-settings`, {
-      // next: { revalidate: 3600 },
-    });
+    const response = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/site-settings`);
     
     if (!response.ok) {
       throw new Error('Failed to fetch site settings');
@@ -20,7 +18,9 @@ export async function getSiteSettings() {
 
 export async function getPageData(endpoint) {
   try {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}${endpoint}`);
+    const response = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}${endpoint}`, {
+      next: { revalidate: revalidateTime },
+    });
     
     if (!response.ok) {
       throw new Error(`Failed to fetch data from ${endpoint}`);
